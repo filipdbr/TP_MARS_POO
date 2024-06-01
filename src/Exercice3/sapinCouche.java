6ce8bdf81f09c691ca4ffa3876ba6243ec90cc80
Filip <filip.a.dabrowski@gmail.com>
2024-06-01 18:07:47 +0200

    Final Version of sapin couche

---
+++ src/Exercice3/sapinCouche.java
@@ -0,0 +1,40 @@
+package Exercice3;
+
+import java.util.Scanner;
+
+public class sapinCouche {
+    public static void main(String[] args) {
+
+        System.out.println("Saisissez la hauteur du sapin (superieur a 1) : ");
+
+        try {
+
+            Scanner scanner = new Scanner(System.in);
+
+            int h = scanner.nextInt();
+
+            if (h <= 1) {
+                System.out.println("Le nombre doit être supérieur à 1");
+                return;
+            }
+
+            scanner.close();
+
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < i + 1; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < h - i - 1; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+
+        } catch (Exception e) {
+            System.out.println("Vous n'avez pas saisi un nombre valide");
+        }
+    }
+}
