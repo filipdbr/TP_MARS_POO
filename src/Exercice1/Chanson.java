4e107d42d05b5b1f2ae716836f0a82a4b45088d4
Filip <filip.a.dabrowski@gmail.com>
2024-06-01 12:10:40 +0200

    Final version

---
+++ src/Exercice1/Chanson.java
@@ -0,0 +1,11 @@
+package Exercice1;
+
+public class Chanson {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 15; i++) {
+            System.out.println(i + " kilomètre" + (i > 1 ? "s" : "") + "à pied, ça use, ça use");
+            System.out.println(i + " kilomètre" + (i > 1 ? "s" : "") + "à pied ça use les souliers.");
+        }
+    }
+}
+
